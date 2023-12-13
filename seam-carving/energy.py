adbfd643f8c9baa176e96a0e0dac9676bdbf3d98
95tuanle <tlna95@icloud.com>
2023-12-13 17:59:02 -0500

    calculate energy

--- seam-carving/energy.py
+++ seam-carving/energy.py
@@ -14,7 +14,24 @@ import sys
 from utils import Color, read_image_into_array, write_array_into_image
 
 
-def energy_at(pixels, x, y):
+def calculate_diff_and_square(pixels_array, coord1, coord2, attr):
+    """
+    Helper function to calculate the difference and square of RGB values.
+    """
+    diff = getattr(pixels_array[coord1[0]][coord1[1]], attr) - getattr(pixels_array[coord2[0]][coord2[1]], attr)
+    return diff * diff
+
+
+def boundary_condition(value, max_value):
+    """
+    Helper function to handle boundary conditions.
+    """
+    value0 = value if value == 0 else value - 1
+    value1 = value if value == max_value - 1 else value + 1
+    return value0, value1
+
+
+def energy_at(pixels_array, x, y):
     """
     Compute the energy of the image at the given (x, y) position.
 
@@ -26,11 +43,19 @@ def energy_at(pixels, x, y):
     This is one of the functions you will need to implement. Expected return
     value: a single number representing the energy at that point.
     """
+    h = len(pixels_array)
+    w = len(pixels_array[0])
 
-    raise NotImplementedError('energy_at is not implemented')
+    x0, x1 = boundary_condition(x, w)
+    dx = sum(calculate_diff_and_square(pixels_array, (y, x0), (y, x1), attr) for attr in ('r', 'g', 'b'))
+
+    y0, y1 = boundary_condition(y, h)
+    dy = sum(calculate_diff_and_square(pixels_array, (y0, x), (y1, x), attr) for attr in ('r', 'g', 'b'))
+
+    return dx + dy
 
 
-def compute_energy(pixels):
+def compute_energy(pixels_array):
     """
     Compute the energy of the image at every pixel. Should use the `energy_at`
     function to actually compute the energy at any single position.
@@ -43,13 +68,20 @@ def compute_energy(pixels):
     value: the 2D grid of energy values.
     """
 
-    raise NotImplementedError('compute_energy is not implemented')
+    energy = [[0 for _ in row] for row in pixels_array]
+
+    for y, row in enumerate(pixels_array):
+        for x, _ in enumerate(row):
+            energy[y][x] = energy_at(pixels_array, x, y)
+
+    return energy
 
 
-def energy_data_to_colors(energy_data):
+def energy_data_to_colors(energy_values):
     """
     Convert the energy values at each pixel into colors that can be used to
-    visualize the energy of the image. The steps to do this conversion are:
+    visualize the energy of the image.
+    The steps to take this conversion are:
 
       1. Normalize the energy values to be between 0 and 255.
       2. Convert these values into grayscale colors, where the RGB values are
@@ -58,22 +90,14 @@ def energy_data_to_colors(energy_data):
     This is NOT one of the functions you have to implement.
     """
 
-    colors = [[0 for _ in row] for row in energy_data]
+    colors = [[0 for _ in row] for row in energy_values]
 
-    max_energy = max(
-        energy
-        for row in energy_data
-        for energy in row
-    )
+    max_energy = max(energy for row in energy_values for energy in row)
 
-    for y, row in enumerate(energy_data):
+    for y, row in enumerate(energy_values):
         for x, energy in enumerate(row):
             energy_normalized = round(energy / max_energy * 255)
-            colors[y][x] = Color(
-                energy_normalized,
-                energy_normalized,
-                energy_normalized
-            )
+            colors[y][x] = Color(energy_normalized, energy_normalized, energy_normalized)
 
     return colors
 
